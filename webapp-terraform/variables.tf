d7ae1dc6e14eadd85f228a417e92804895bc21c9
hunqba <hunqba@hotmail.com>
2022-12-29 14:42:11 +0700

    Add terraform source code to provisioning EKS & VPC

--- webapp-terraform/variables.tf
+++ webapp-terraform/variables.tf
@@ -3,3 +3,54 @@ variable "region" {
   type        = string
   default     = "ap-southeast-1"
 }
+variable "vpc_name" {
+  description = "Name of VPC"
+  type        = string
+  default     = "webapp-vpc"
+}
+
+variable "vpc_cidr" {
+  description = "CIDR block for VPC"
+  type        = string
+  default     = "10.0.0.0/16"
+}
+variable "vpc_azs" {
+  description = "Availability zones for VPC"
+  type        = list(string)
+  default     = ["ap-southeast-1a", "ap-southeast-1b"]
+}
+variable "vpc_private_subnets" {
+  description = "Private subnets for VPC"
+  type        = list(string)
+  default     = ["10.0.1.0/24", "10.0.2.0/24"]
+}
+
+variable "vpc_public_subnets" {
+  description = "Public subnets for VPC"
+  type        = list(string)
+  default     = ["10.0.101.0/24", "10.0.102.0/24"]
+}
+variable "vpc_enable_nat_gateway" {
+  description = "Enable NAT gateway for VPC"
+  type        = bool
+  default     = true
+}
+variable "vpc_enable_dns_hostnames" {
+  description = "Enable DNS hostnames for VPC"
+  type        = bool
+  default     = true
+}
+variable "vpc_tags" {
+  description = "Tags to apply to resources created by VPC module"
+  type        = map(string)
+  default = {
+    Terraform   = "true"
+    Environment = "dev"
+  }
+}
+variable "node_instance_types" {
+  description = "Instance type of node group"
+  type        = string
+  default     = "t3.small"
+}
+
